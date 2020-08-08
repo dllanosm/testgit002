a7d783b9f89d151bf8a4af3b339ce54d6fbee85a
Daniel Llanos Meza <dllanos@farmaciasperuanas.pe>
2020-08-07 21:17:01 -0500

    add new file

---
+++ class001.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace testconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Hi All!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
